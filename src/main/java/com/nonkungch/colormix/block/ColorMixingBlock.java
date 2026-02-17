08cf4291a8618c78b75c2f81e4f673ea5d8071cb
NonKungCh. <116896756+NonKun1122@users.noreply.github.com>
2026-02-17 20:23:47 +0700

    Add files via upload

---
+++ src/main/java/com/nonkungch/colormix/block/ColorMixingBlock.java
@@ -0,0 +1,51 @@
+package com.nonkungch.colormix.block;
+
+import com.nonkungch.colormix.block.entity.ColorMixingBlockEntity;
+import com.nonkungch.colormix.block.entity.ModBlockEntities;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class ColorMixingBlock extends BlockWithEntity implements BlockEntityProvider {
+    public ColorMixingBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new ColorMixingBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return validateTicker(type, ModBlockEntities.COLOR_MIXING_BLOCK_ENTITY,
+                (world1, pos, state1, blockEntity) -> blockEntity.tick());
+    }
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (!world.isClient) {
+            NamedScreenHandlerFactory screenHandlerFactory = (NamedScreenHandlerFactory) world.getBlockEntity(pos);
+
+            if (screenHandlerFactory != null) {
+                player.openHandledScreen(screenHandlerFactory);
+            }
+        }
+        return ActionResult.SUCCESS;
+    }
+}
